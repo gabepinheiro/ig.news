5421f3e4e2d6900396a073d60f1ef2649e3c5665
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-18 00:10:57 -0300

    add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início - ig.news</title>
       </Head>
-      <h1 className={styles.title}>Ignews</h1>
+      <h1>Ignews</h1>
     </>
   )
 }
