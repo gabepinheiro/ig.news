f9ad6200debcbe80f1488d0d19f5e014e149a47c
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-17 23:51:53 -0300

    add head and title to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Ignews</h1>
+    <>
+      <Head>
+        <title>Início - ig.news</title>
+      </Head>
+      <h1 className={styles.title}>Ignews</h1>
+    </>
   )
 }
