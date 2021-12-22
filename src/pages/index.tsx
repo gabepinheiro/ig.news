17830fc473e876403369e409b14c99cb26bc5dcc
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-21 23:25:54 -0300

    change getServerSideProps to getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 import {stripe} from '../services/stripe'
 import {HomeTemplate} from '../templates/home'
 
@@ -16,7 +16,7 @@ export default function Home({product}: HomeProps) {
 
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1K8Ex5F2dE7RQsnHkml6sMmd')
 
   const product = {
@@ -31,5 +31,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product
     },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
